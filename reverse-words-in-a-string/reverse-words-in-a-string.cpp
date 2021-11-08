e7828137891364e15e15c08b042e99217fc33e33
Abhinash Bhengra <49307371+abhinashbhengra@users.noreply.github.com>
2021-11-08 21:31:39 +0530

    Time: 8 ms (46.31%), Space: 20.8 MB (18.05%) - LeetHub

---
+++ reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res;
+        string temp;
+        
+        for(int i{0}; s[i] != '\0'; i++){
+            if(s[i] != ' '){
+                temp += s[i];
+            }
+            else{
+                if(temp.size())
+                    res = temp+ ' ' + res;
+                temp.clear();
+            }
+        }
+        if(temp.size())
+            res = temp + ' ' + res;
+        
+        res.erase(res.size()-1);
+        return res;
+    }
+};
